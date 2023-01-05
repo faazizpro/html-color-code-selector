cd6974c5af0a552ca98db98a088c89ed78d67111
faazizpro <fahadafs11@gmail.com>
2023-01-05 16:04:20 +0600

    Red HTML Code Done

--- JS/ColorChange.js
+++ JS/ColorChange.js
@@ -18,3 +18,55 @@ document.getElementById('btnCopy').addEventListener('click', function(){
 
 
 /*---------------------------Indian Red---------------------*/
+//Color Fill for LightCoral
+document.getElementById('lightCoralChange').addEventListener('click', function(){
+    document.getElementById('lightCoralFill').style.backgroundColor = '#F08080';
+});
+
+//Copy Text for LightCoral
+document.getElementById('btnLightCoral').addEventListener('click', function(){
+    const text = document.getElementById('copyLightCoral');
+    const copytext = text.innerText;
+
+    //Now copy the text for IndianRed
+    navigator.clipboard.writeText(copytext);
+
+    //Show alert for IndianRed
+    alert(`Code ${copytext} copied`);
+});
+
+/*---------------------------Salmon---------------------*/
+//Color Fill for Salmon
+document.getElementById('salmonChange').addEventListener('click', function(){
+    document.getElementById('salmonFill').style.backgroundColor = '#FA8072';
+});
+
+//Copy Text for Salmon
+document.getElementById('btnSalmon').addEventListener('click', function(){
+    const text = document.getElementById('copySalmon');
+    const copytext = text.innerText;
+
+    //Now copy the text for IndianRed
+    navigator.clipboard.writeText(copytext);
+
+    //Show alert for IndianRed
+    alert(`Code ${copytext} copied`);
+});
+
+/*---------------------------DarkSalmon---------------------*/
+//Color Fill for DarkSalmon
+document.getElementById('DarkSalmonChange').addEventListener('click', function(){
+    document.getElementById('DarkSalmonFill').style.backgroundColor = '#E9967A';
+});
+
+//Copy Text for DarkSalmon
+document.getElementById('btnDarkSalmon').addEventListener('click', function(){
+    const text = document.getElementById('copyDarkSalmon');
+    const copytext = text.innerText;
+
+    //Now copy the text for DarkSalmon
+    navigator.clipboard.writeText(copytext);
+
+    //Show alert for DarkSalmon
+    alert(`Code ${copytext} copied`);
+});
